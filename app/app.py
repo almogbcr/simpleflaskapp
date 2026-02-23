7d913cfe65b6474d0f49b920887b2ed8985e86f1
Almog Bachar <almog@tivtaam.co.il>
2026-02-23 09:18:00 +0200

    init

---
+++ app/app.py
@@ -0,0 +1,40 @@
+from flask import Flask,jsonify,render_template_string
+import os
+import datetime
+
+
+app = Flask(__name__)
+
+##minimal HTML
+HTML_INDEX = """
+<!doctype html>
+<html>
+<head>
+    <title>Simple Flask App</title>
+</head>
+<body>
+    <h1>Simple Flask App Running ✅</h1>
+    <p>Host: {{ host }}</p>
+    <p>Time: {{ time }}</p>
+</body>
+</html>
+"""
+
+@app.route("/")
+def index():
+    return render_template_string(
+        HTML_INDEX,
+        host=os.getenv("HOSTNAME" , "local"),
+        time=datetime.datetime.now()
+    
+    )
+@app.route("/health")
+def health():
+    return jsonify({
+        "status":"ok",
+        "service":"simple-flaskapp",
+        "time": datetime.datetime.now().isoformat()
+    }), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=8000)
